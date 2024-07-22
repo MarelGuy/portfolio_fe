ec6a48758a8d971fda60493b3ab3430c0365bf1e
--global <irertnuc.loris@gmail.com>
2024-07-22 03:53:26 +0200

    added page orientation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
     import { page } from "$app/stores";
+    import { whichPage } from "../scripts/global";
+
+    import "../styles/global.scss";
 
     import Navbar from "../components/Navbar.svelte";
+
+    page.subscribe(async (value) => {
+        if (value.route && value.route.id)
+            whichPage.set(
+                value.route.id === "/" ? "home" : value.route.id.substring(1),
+            );
+    });
 </script>
 
 <body>
