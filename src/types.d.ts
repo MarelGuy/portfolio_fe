568c24e2a373ebbf21fad012510125978fef1d0c
--global <l.cuntreri@gosport.tech>
2024-07-22 03:16:28 +0200

    updated configuration

---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    // ... other env variables
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,20 @@
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 import adapter from '@sveltejs/adapter-netlify';
+import { sveltePreprocess } from 'svelte-preprocess';
+import { loadEnv } from 'vite';
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd());
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: [vitePreprocess({})],
-
+  plugins: [sveltekit()],
+  preprocess: [sveltePreprocess({})],
+  define: {
+    'process.env': process.env,
+  },
   kit: {
-    // default options are shown
     adapter: adapter({
-      // if true, will create a Netlify Edge Function rather
-      // than using standard Node-based functions
       edge: false,
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app.
-      // if `edge` is true, this option cannot be used
       split: false
     })
   }
